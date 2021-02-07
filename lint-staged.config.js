0d82b1bf7517da66e946e93dd715dc796f50e726
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2021-02-07 16:23:34 +0900

    stagedではないファイルも型チェックの対象になっていたので修正

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     '**/*.ts': (fileNames) => [
-        'tsc -p tsconfig-lint-staged.json --noEmit',
+        // for Linux only
+        // `tsc --showConfig -p tsconfig-lint-staged.json | jq -j '.compilerOptions | to_entries | map("--" + .key + " " + (.value | tostring)) | @sh' | xargs -t -I {} sh -c 'npx tsc --noEmit {} ${fileNames.join(' ')}'`,
+        // for Windows (Linuxでも動く)
+        `tsc --showConfig -p tsconfig-lint-staged.json | jq -j ".compilerOptions | to_entries | map(\\"--\\" + .key + \\" \\" + (.value | tostring)) | @sh" | xargs -t -I {} sh -c 'npx tsc --noEmit {} ${fileNames.join(' ')}'`,
         `eslint ${fileNames.join(' ')} --fix`,
         `prettier --write ${fileNames.join(' ')}`,
     ],
