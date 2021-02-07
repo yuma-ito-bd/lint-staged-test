0bb534627408a7b23a6c5caaa9728306ac0accb5
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2021-02-07 18:36:20 +0900

    コマンドを分割して少し分かりやすく

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,13 @@
 module.exports = {
     '**/*.ts': (fileNames) => [
         // for Linux only
-        // `tsc --showConfig -p tsconfig-lint-staged.json | jq -j '.compilerOptions | to_entries | map("--" + .key + " " + (.value | if type=="array" then join(",") else . end | tostring)) | @sh' | xargs -t -I {} sh -c 'npx tsc --noEmit {} ${fileNames.join(' ')}'`,
+        // `tsc --showConfig -p tsconfig-lint-staged.json | \
+        // jq -j '.compilerOptions | to_entries | map("--" + .key + " " + (.value | if type=="array" then join(",") else . end | tostring)) | @sh' | \
+        // xargs -t -I {} sh -c 'tsc --noEmit {} ${fileNames.join(' ')}'`,
         // for Windows (Linuxでも動く)
-        `tsc --showConfig -p tsconfig-lint-staged.json | jq -j ".compilerOptions | to_entries | map(\\"--\\" + .key + \\" \\" + (.value | if type==\\"array\\" then join(\\",\\") else . end | tostring)) | @sh" | xargs -t -I {} sh -c 'npx tsc --noEmit {} ${fileNames.join(' ')}'`,
+        `tsc --showConfig -p tsconfig-lint-staged.json | \
+        jq -j ".compilerOptions | to_entries | map(\\"--\\" + .key + \\" \\" + (.value | if type==\\"array\\" then join(\\",\\") else . end | tostring)) | @sh" | \
+        xargs -t -I {} sh -c 'tsc --noEmit {} ${fileNames.join(' ')}'`,
         `eslint ${fileNames.join(' ')} --fix`,
         `prettier --write ${fileNames.join(' ')}`,
     ],
