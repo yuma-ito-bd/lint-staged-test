a5b1134f85debb1dc33eb45b928e135700ed83f2
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2021-02-07 18:38:04 +0900

    サンプル用ファイルを追加

---
+++ src/EslintErrorExample.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let num = 1;
---
+++ src/TsconfigErrorExample.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+const numberList = [5, 12, 8, 130, 44];
+const found: number = numberList.find((element) => element > 10);
+
+const str1: string = undefined;
